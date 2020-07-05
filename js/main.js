972648589d71798deda9549f8ee51d5d4a73d7ef
Tarlan Soltanov <soltanov.terlan04@outlook.com>
2020-07-05 17:01:26 +0400

    version 1

--- js/main.js
+++ js/main.js
@@ -14,4 +14,37 @@ button.addEventListener('click', () => {
     }
     counter = !counter;
 
-})
+});
+$('.slider_inner .owl-carousel').owlCarousel({
+    loop: true,
+    margin: 30,
+    nav: true,
+    lazyLoad: true,
+    responsive: {
+        0: {
+            center: false,
+            items: 1,
+            dots: true,
+            slideBy: 1,
+            dotsEach: 1,
+
+        },
+        768: {
+            center: false,
+            items: 1,
+            dots: true,
+            margin: 30,
+            stagePadding: 0,
+            slideBy: 1,
+            dotsEach: 1,
+
+        },
+        1024: {
+            center: true,
+            items: 1,
+            slideBy: 1,
+            dotsEach: 1,
+        }
+
+    }
+});
